5545a19353bb142e0af831c119170c699795e8fc
Michael Barton <michael.barton10@gmail.com>
2012-04-16 21:28:29 +0100

    Added index page and a link to a new random room

--- client/js/matrix.js
+++ client/js/matrix.js
@@ -1,17 +1,17 @@
-// var matrix = [
-// 	{"sample": "kick", "path": "samples/kick.mp3",
-// 	 "triggers": [true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false]},
-// 	{"sample": "hat", "path": "samples/hat.mp3",
-// 	 "triggers": [false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false]},
-// 	{"sample": "snare", "path": "samples/snare.mp3",
-// 	 "triggers": [false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, false]},
-// 	{"sample": "crash", "path": "samples/crash.mp3",
-// 	 "triggers": [true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]}
-// ];
+var matrix = [
+	{"sample": "kick", "path": "samples/kick.mp3",
+	 "triggers": [true, false, false, false, true, false, false, false, true, false, false, false, true, false, false, false]},
+	{"sample": "hat", "path": "samples/hat.mp3",
+	 "triggers": [false, false, true, false, false, false, true, false, false, false, true, false, false, false, true, false]},
+	{"sample": "snare", "path": "samples/snare.mp3",
+	 "triggers": [false, false, false, false, true, false, false, false, false, false, false, false, true, false, false, false]},
+	{"sample": "crash", "path": "samples/crash.mp3",
+	 "triggers": [true, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]}
+];
 
-// var patternLength = matrix[0]["triggers"].length;
-var matrix = [];
-var patternLength = 0;
+var patternLength = matrix[0]["triggers"].length;
+// var matrix = [];
+// var patternLength = 0;
 
 var samples = [
 	// {"id": "kick", "path": "samples/kick.mp3"},
