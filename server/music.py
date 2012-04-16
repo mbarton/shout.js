feba99397b8a9b6a3d3178bbc99feae0510e1f18
Kiril Piskunov <ciudilo@gmail.com>
2012-04-16 18:14:36 +0000

    addidional endpoints for adding samples

--- server/music.py
+++ server/music.py
@@ -10,7 +10,7 @@ pusher.key = '79a35f6a8db88187adb6'
 pusher.secret = 'a719ef391208056aa57c'
 app.p = pusher.Pusher()
 
-#positions = [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
+inital_sample_state = [False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False]
 positions = [
 {"sample": "kick", "path": "samples/kick.mp3", "triggers": [True, False, False, False, True, False, False, False, True, False, False, False, True, False, False, False]},
 {"sample": "hat", "path": "samples/hat.mp3", "triggers": [False, False, True, False, False, False, True, False, False, False, True, False, False, False, True, False]},
@@ -25,6 +25,8 @@ class AutoVivification(dict):
         except KeyError:
             value = self[item] = type(self)()
             return value
+    def get_dict():
+      return dict
 
 rooms = AutoVivification()
 
@@ -32,6 +34,7 @@ rooms = AutoVivification()
 def getstate(room):
     if room not in rooms:
       rooms[room] = positions 
+    print json.dumps(rooms)
     callback = request.args.get('callback', False)
     if callback:
       content = str(callback) + '(' + json.dumps(rooms[room]) + ')'
@@ -40,24 +43,45 @@ def getstate(room):
     return json.dumps(rooms[room])
 
 @app.route('/change/<room>/<sample>/<position>/<enabled>')
-def hello_world(room, sample, position, enabled):
-    sample_i = get_sample_i(sample)
+def update_room_state(room, sample, position, enabled):
+    sample_i = get_sample_index(room, sample)
+    print 'Updating sample' + str(sample_i)
     rooms[room][sample_i]['triggers'][int(position)] = bool(enabled)
      
-    app.p[room].trigger('an_event', {'sample': sample, 'position': position, 'enabled': enabled})
+    app.p[room].trigger('change', {'sample': sample, 'position': position, 'enabled': enabled})
     #rooms[room] = {'sample': sample, 'positions': positions}
     print rooms
     return ''
 
-def get_sample_i(sample):
-  if sample == 'kick':
-    return 0
-  if sample == 'hat':
-    return 1
-  if sample == 'snare':
-    return 2
-  if sample == 'crash':
-    return 3
+@app.route('/addsample/<room>/<name>', methods=['POST'])
+def add_sample(room, name):
+  print len(rooms[room]) #{'sample': name, 'path': path}
+  file_path = request.form.get('filepath', False)
+  if get_sample_index(room, name) == -1:
+    new_triggers = []
+    new_triggers += inital_sample_state
+    new_sample_json = {'sample': name, 'path': file_path, 'triggers': new_triggers}
+    rooms[room].append(new_sample_json)
+    app.p[room].trigger('newsample', new_sample_json)
+    return 'Sample has been added'
+  return 'Sample is a duplicate'
+
+@app.route('/playback/<room>/<state>')
+def update_playback(room, state):
+  app.p[room].trigger('playback', {'state': state})
+  return ''
+
+@app.route('/tempo/<room>/<tempo>')
+def update_tempo(room, tempo):
+  app.p[room].trigger('tempo', {'tempo' : tempo})
+  return ''
+
+def get_sample_index(room, sample):
+  for room_sample in rooms[room]:
+     if room_sample['sample'] == sample:
+	#print 'Got index for ' + sample + ' ' + str(rooms[room].index(room_sample))
+        return rooms[room].index(room_sample)
+  return -1
 
 @app.route('/')
 def catch_all():
